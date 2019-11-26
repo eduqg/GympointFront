6495a685f07964876d42b8b509d97f940df6a1d2
eduqg <eduardoqgomes@gmail.com>
2019-11-26 08:59:13 -0300

    Sandwich for small screens

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,19 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
+import { MdList } from 'react-icons/md';
 import { signOut } from '../../store/modules/auth/actions';
 
-import { Container, Content, Profile } from './styles';
+import {
+  Container,
+  Content,
+  Profile,
+  SandwichButton,
+  SandwichOptions,
+  LogoLeft,
+  NavSmall,
+} from './styles';
 import logo from '../../assets/halter.png';
 
 import { store } from '../../store';
@@ -12,11 +21,16 @@ import { store } from '../../store';
 export default function Header() {
   const dispatch = useDispatch();
   const { signed } = useSelector(state => state.auth);
+  const [showSandwich, setShowSandwich] = useState(false);
 
   const authName = useMemo(() => store.getState().auth.name, [signed]); // eslint-disable-line
 
   function handleSignOut() {
     dispatch(signOut());
+  }
+
+  function toggleSandwich() {
+    setShowSandwich(!showSandwich);
   }
 
   return (
@@ -48,6 +62,38 @@ export default function Header() {
           </button>
         </Profile>
       </Content>
+      <NavSmall>
+        <LogoLeft>
+          <img src={logo} alt="gympointlogo" />
+          <NavLink to="/" className="title">
+            GYMPOINT
+          </NavLink>
+        </LogoLeft>
+        <SandwichButton onClick={toggleSandwich}>
+          <MdList color="rgba(0,0,0,0.4)" size={35} />
+        </SandwichButton>
+        {showSandwich && (
+          <SandwichOptions>
+            <NavLink to="/students" activeStyle={{ color: '#333' }}>
+              ALUNOS
+            </NavLink>
+            <NavLink to="/plans" activeStyle={{ color: '#333' }}>
+              PLANOS
+            </NavLink>
+            <NavLink to="/registrations" activeStyle={{ color: '#333' }}>
+              MATRÍCULAS
+            </NavLink>
+            <NavLink to="/help_orders" activeStyle={{ color: '#333' }}>
+              PEDIDOS DE AUXÍLIO
+            </NavLink>
+
+            <p>{authName}</p>
+            <button type="button" onClick={handleSignOut}>
+              Sair do sistema
+            </button>
+          </SandwichOptions>
+        )}
+      </NavSmall>
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -15,6 +15,10 @@ export const Content = styled.div`
   justify-content: space-between;
   align-items: center;
 
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+
   nav {
     display: flex;
     align-items: center;
@@ -64,5 +68,76 @@ export const Profile = styled.div`
     border: 0;
     color: #df4658;
     background: #fff;
+  }
+`;
+
+export const NavSmall = styled.div`
+  @media only screen and (min-width: 769px) {
+    display: none;
+  }
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 65px;
+  justify-content: space-between;
+`;
+
+export const LogoLeft = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 16px;
+  border-right: 1px solid #eee;
+
+  a {
+    font-weight: bold;
+    color: #f84e62;
+    padding-left: 8px;
+  }
+
+  img {
+    height: 30px;
+  }
+`;
+export const SandwichButton = styled.button`
+  background: #fff;
+  border-width: 0px;
+  padding: 5px;
+`;
+
+export const SandwichOptions = styled.div`
+  background: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+
+  position: absolute;
+  top: 50px;
+  right: 10px;
+
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  padding: 10px 30px;
+
+  a {
+    line-height: 40px;
+    font-weight: bold;
+    color: #f84e62;
+  }
+
+  p {
+    border-top: 1px solid rgba(0, 0, 0, 0.1);
+    padding: 10px 0;
+    text-align: center;
+    font-weight: bold;
+  }
+
+  button {
+    background: #f84e62;
+    padding: 8px 0;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    border-radius: 5px;
+    margin-bottom: 10px;
   }
 `;
