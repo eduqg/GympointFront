0cbc4f638d1d2913c6e937151092865f821d1142
eduqg <eduardoqgomes@gmail.com>
2019-11-29 16:47:12 -0300

    Configuring enzyme

---
+++ __tests__/components/Students/initialState.js
@@ -0,0 +1,36 @@
+export const INITIAL_STATE = {
+  student: {
+    allStudents: [
+      {
+        altura: 1.7,
+        createdAt: '2019-10-20T13:15:10.328Z',
+        email: 'gabrielenzo@eu.com',
+        id: 2,
+        idade: 34,
+        name: 'Gabriel Enzo',
+        peso: 80.4,
+        updatedAt: '2019-11-10T11:18:40.728Z',
+      },
+      {
+        altura: 1.5,
+        createdAt: '2019-11-09T20:14:43.240Z',
+        email: 'joao@edu.com',
+        id: 8,
+        idade: 18,
+        name: 'João Medial',
+        peso: 70,
+        updatedAt: '2019-11-10T11:19:12.942Z',
+      },
+      {
+        altura: 1.8,
+        createdAt: '2019-11-09T20:14:28.054Z',
+        email: 'vitor@vitor.com',
+        id: 7,
+        idade: 14,
+        name: 'Vitor Hu Go',
+        peso: 60,
+        updatedAt: '2019-11-10T11:19:42.103Z',
+      },
+    ],
+  },
+};
--- __tests__/components/Students/list.test.js
+++ __tests__/components/Students/list.test.js
@@ -1,68 +1,46 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { mount } from 'enzyme';
 import history from '~/services/history';
 // Cria html 'fake'
 
 // import { addTech } from '~/store/modules/techs/actions';
 import Students from '~/pages/Students/index';
 
+// import INITIAL_STATE from './initialState';
+
 jest.mock('react-redux');
+const dispatch = jest.fn();
+useDispatch.mockReturnValue(dispatch);
 
 describe('List Students component', () => {
   it('should render students', () => {
-    const dispatch = jest.fn();
+    useSelector.mockImplementation(cb =>
+      cb({
+        student: {
+          allStudents: [
+            {
+              altura: 1.7,
+              createdAt: '2019-10-20T13:15:10.328Z',
+              email: 'gabrielenzo@eu.com',
+              id: 2,
+              idade: 34,
+              name: 'Gabriel Enzo',
+              peso: 80.4,
+              updatedAt: '2019-11-10T11:18:40.728Z',
+            },
+          ],
+        },
+      })
+    );
 
-    useDispatch.mockReturnValue(dispatch);
-
-    // useSelector.mockImplementation(cb =>
-    //   cb({
-    //     student: {
-    //       allStudents: [
-    //         {
-    //           altura: 1.7,
-    //           createdAt: '2019-10-20T13:15:10.328Z',
-    //           email: 'gabrielenzo@eu.com',
-    //           id: 2,
-    //           idade: 34,
-    //           name: 'Gabriel Enzo',
-    //           peso: 80.4,
-    //           updatedAt: '2019-11-10T11:18:40.728Z',
-    //         },
-    //         {
-    //           altura: 1.5,
-    //           createdAt: '2019-11-09T20:14:43.240Z',
-    //           email: 'joao@edu.com',
-    //           id: 8,
-    //           idade: 18,
-    //           name: 'João Medial',
-    //           peso: 70,
-    //           updatedAt: '2019-11-10T11:19:12.942Z',
-    //         },
-    //         {
-    //           altura: 1.8,
-    //           createdAt: '2019-11-09T20:14:28.054Z',
-    //           email: 'vitor@vitor.com',
-    //           id: 7,
-    //           idade: 14,
-    //           name: 'Vitor Hu Go',
-    //           peso: 60,
-    //           updatedAt: '2019-11-10T11:19:42.103Z',
-    //         },
-    //       ],
-    //     },
-    //   })
-    // );
-
-    const { getByText, getByTestId, debug } = render(
+    const wrapper = mount(
       <Router history={history}>
         <Students />
       </Router>
     );
-
-    debug();
 
     expect(dispatch).toHaveBeenCalledWith({
       type: '@student/LOAD_ALL_STUDENTS_REQUEST',
